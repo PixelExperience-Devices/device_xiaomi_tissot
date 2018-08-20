3607ffb2b9548ba6db023cd9117adb08f1cf41d5
Hridya Valsaraju <hridya@google.com>
2018-08-20 16:31:22 -0700

    tissot: Build libgptutils with Android.bp
    This is required to build bootctrl.msm8953 with Android.bp.
    
    Bug: 78793464
    Test: make
    
    Change-Id: Iff4f50ad363cc003d60a5380addd79749b547514

--- device.mk
+++ device.mk
@@ -53,6 +53,12 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES_DEBUG += \
     bootctl
 
+PRODUCT_STATIC_BOOT_CONTROL_HAL := \
+    bootctrl.msm8953 \
+    libcutils \
+    libgptutils.tissot.recovery \
+    libz
+
 # Audio configuration
 PRODUCT_COPY_FILES += \
     $(LOCAL_PATH)/audio/audio_platform_info.xml:$(TARGET_COPY_OUT_VENDOR)/etc/audio_platform_info.xml \
