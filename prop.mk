757f9772defdf495f42114d3873b9ba47f9519ce
Artem Borisov <dedsa2002@gmail.com>
2019-04-29 18:43:39 +0530

    tissot: Re-enable fluence
    * In-call audio is ugly without fluence, I'm not sure why stock keeps it disabled.
    
    Change-Id: I8b59a312a7f5d16c5472d4669446287791d8de8b

--- prop.mk
+++ prop.mk
@@ -3,7 +3,7 @@ PRODUCT_PROPERTY_OVERRIDES += \
 persist.vendor.audio.fluence.speaker=true \
 persist.vendor.audio.fluence.voicecall=true \
 persist.vendor.audio.fluence.voicerec=false \
-ro.vendor.audio.sdk.fluencetype=none
+ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera,com.dual.GCam,com.Tele.GCam
