927cf3e0e0e7d5eefd337e97e00f22c8eb21058e
abhinavgupta371 <abhinavgupta371@gmail.com>
2019-12-10 19:29:04 +0530

    tissot: Ship prebuilt Twrp

---
+++ twrp/README.md
@@ -0,0 +1,1 @@
+Pick before building <https://gerrit.aicp-rom.com/c/AICP/build/+/94064>
---
+++ twrp/twrp.mk
@@ -0,0 +1,5 @@
+TWRP_PATH := device/xiaomi/tissot/twrp
+
+TARGET_PREBUILT_RECOVERY_RAMDISK_IMG := $(TWRP_PATH)/ramdisk-recovery.img
+TARGET_PREBUILT_RECOVERY_RAMDISK := $(TWRP_PATH)/ramdisk-recovery.zip
+
