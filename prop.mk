2d37ea0c13c73411a877d44caa0d1a9463f1ca7f
abhinavgupta371 <abhinavgupta371@gmail.com>
2020-01-09 12:49:45 +0000

    tissot: Remove aux blacklist

--- prop.mk
+++ prop.mk
@@ -7,7 +7,10 @@ ro.vendor.audio.sdk.fluencetype=fluence
 
 # Camera
 PRODUCT_PROPERTY_OVERRIDES += \
-vendor.camera.aux.packageblacklist=com.google.android.GoogleCamera,com.discord \
+vendor.camera.aux.packageblacklist=com.discord \
+persist.camera.HAL3.enabled=1 \
+vendor.camera.aux.packagelist=org.codeaurora.snapcam,com.android.camera2,com.google.android.GoogleCamera \
+vendor.camera.aux.packagelist2=com.google.android.GoogleCameraWide,com.dual.GCam,com.Wide.GCam,com.Tele.GCam \
 vendor.camera.hal1.packagelist=com.whatsapp \
 persist.camera.dual.camera=0 \
 persist.camera.eis.enable=1
