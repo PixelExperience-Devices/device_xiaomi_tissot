deb95e9a5b727147e1486c107819de16af726a52
abhinavgupta371 <abhinavgupta371@gmail.com>
2021-03-10 17:41:06 +0000

    tissot: Drop overlay-lineage
     Add AOSP overlays as well

--- device.mk
+++ device.mk
@@ -11,7 +11,6 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk
 
 # Overlay
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
-DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay-lineage
 
 # Boot animation
 TARGET_SCREEN_HEIGHT := 1920
